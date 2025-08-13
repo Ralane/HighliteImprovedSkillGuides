615145394b973c1ec79f68b3c59a6e3748b2adae
Riley Lane <rileylanea@gmail.com>
2025-08-13 16:51:36 -0700

    Added hover tooltips. Added Recipe formatting.

--- src/ImprovedSkillGuides.ts
+++ src/ImprovedSkillGuides.ts
@@ -1,4 +1,4 @@
-import {Plugin, SettingsTypes} from "@highlite/plugin-api";
+import {Plugin, SettingsTypes, UIManager } from "@highlite/plugin-api";
 import { PanelManager } from "@highlite/plugin-api";
 import lookupTable from './lookupTable.json';
 
@@ -6,6 +6,9 @@ export default class ImprovedSkillGuides extends Plugin {
     panelManager: PanelManager = new PanelManager();
     pluginName = "ImprovedSkillGuides";
     author: string = "0rangeYouGlad";
+    private uiManager = new UIManager();
+    private currentTooltip: { hide: () => void } | null = null;
+    private currentTooltipAnchor: HTMLElement | null = null;
 
     constructor() {
         super();
@@ -42,9 +45,16 @@ export default class ImprovedSkillGuides extends Plugin {
         };
 
         this.settings.showItemEffects = {
-            text: "Show Item Details",
+            text: "Show Misc Notes",
             type: SettingsTypes.checkbox,
             value: false,
+            callback: () => {},
+        };
+
+        this.settings.showItemTooltips = {
+            text: "Show Output Item Tooltips",
+            type: SettingsTypes.checkbox,
+            value: true,
             callback: () => {},
         };
     };
@@ -58,14 +68,57 @@ export default class ImprovedSkillGuides extends Plugin {
 
     stop(): void {
         this.log("ImprovedSkillGuides stopped");
+
+        this.hideTooltip();
     } 
+
+    private hideTooltip() {
+        if (this.currentTooltip) {
+            this.currentTooltip.hide();
+            this.currentTooltip = null;
+        }
+    }
+
+    private showTooltip(anchor: HTMLElement, id: number) {
+        if (this.currentTooltip) {
+            this.currentTooltip.hide();
+        }
+
+        if (isNaN(id)) return;
+
+        this.currentTooltipAnchor = anchor;
+
+        // Use coordinates from the mouse event if available, otherwise use element position
+        let x = 0;
+        let y = 0;
+
+        const rect = anchor?.getBoundingClientRect();
+        x = rect.right;
+        y = rect.top;
+
+        // Use UIManager to draw the tooltip
+        try {
+           this.currentTooltip = this.uiManager.drawItemTooltip(id, x, y);
+        } catch (error) {
+            this.log(`Error showing tooltip: ${error}`);
+        }
+    }
 
     // TODO - move this into a manager like https://github.com/Highl1te/Core/commit/a9011e05b0a4a410e4f8a9a3dbd9873e92a0d4c1
     GameLoop_update() {
         let skillMenu = document.getElementById("hs-skill-guide-menu");
-        if(!skillMenu) return;
+    
+
+        if(!skillMenu) {
+            this.hideTooltip();
+            return
+        };
         
         let childSkillEntries = Array.from(skillMenu.getElementsByClassName("hs-unlockable-skill-panel"));
+
+        if(!childSkillEntries.length || !this.currentTooltipAnchor || !this.currentTooltipAnchor.checkVisibility()) {
+            this.hideTooltip();
+        }
 
         childSkillEntries.forEach((child) => {
             let subject = child.childNodes[1].childNodes[0].textContent || ""; 
@@ -75,8 +128,58 @@ export default class ImprovedSkillGuides extends Plugin {
 
             Object.entries(lookupTable[subject]).forEach((line) => {
 
-                if(line[0] === "recipe" && !this.settings.showRecipe.value) {
-                    // this.log("Skipping Recipe");
+                let textContent = '';
+
+                if(line[0] === "itemID") {
+                    if(!this.settings.showItemTooltips.value) {
+                        // this.log("Skipping Item Tooltip");
+                    }
+                    else {
+                        child.addEventListener('mouseenter', e =>
+                         this.showTooltip(child, line[1])
+                     );
+                        child.addEventListener('mouseleave', () => this.hideTooltip());
+                    }
+                    
+                }
+                else if(line[0] === "recipe") {
+                    if(!this.settings.showRecipe.value) {
+                        // this.log("Skipping Recipe");
+                    }
+                    else {
+
+                        // this.log("Showing recipe for " + lookupTable[subject]["itemID"]);
+
+                        const itemDef =
+                        this.gameHooks.ItemDefinitionManager._itemDefMap.get(
+                            lookupTable[subject]["itemID"]
+                        );
+
+                        // Recipe (if item has crafting recipe)
+                        if (itemDef._recipe && itemDef._recipe._ingredients && itemDef._recipe._ingredients.length > 0) {
+                            itemDef._recipe._ingredients.forEach((ingredient: any) => {
+                                const ingredientDiv = document.createElement('div');
+                                ingredientDiv.className = 'hs-ui-item-tooltip-effect';
+                                if(textContent) {
+                                    textContent += " ";
+                                }
+                                try {
+                                    const ingredientDef = this.gameHooks.ItemDefinitionManager._itemDefMap.get(ingredient._itemId);
+                                    const ingredientName = ingredientDef?._nameCapitalized || ingredientDef?._name || `Item ${ingredient._itemId}`;
+                                    textContent += `${ingredient._amount}x ${ingredientName}`;
+                                } catch {
+                                    textContent += `${ingredient._amount}x Item ${ingredient._itemId}`;
+                                }
+                            });
+                        }
+
+                        if(!textContent) {
+                            textContent = `${line[1]}`; // Recipe fallback
+                        }
+
+
+
+                    }
                 } else if(line[0] === "xp" && !this.settings.showXp.value) {
                     // this.log("Skipping XP");
                 } else if(line[0] === "facility" && !this.settings.showFacility.value) {
@@ -86,11 +189,17 @@ export default class ImprovedSkillGuides extends Plugin {
                 } else if(line[0] === "itemEffects" && !this.settings.showItemEffects.value) {
                     // this.log("Skipping Item Effects");
                 } else {
-                    // this.log("Creating for " + subject);
+
+                    textContent = `${line[1]}`;
+                }
+
+                
+                if(textContent) {
+                                        // this.log("Creating for " + subject);
                     let newText = document.createElement("span");
                     newText.className = "hs-text--yellow";
                     newText.style = "color: rgb(240, 230, 140) !important;";
-                    newText.innerText = `${line[1]}`;
+                    newText.innerText = `${textContent}`;
                     child.childNodes[1].appendChild(newText);
                 }
             })
